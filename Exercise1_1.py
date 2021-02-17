b705135f59e16dff9638679e88e40fa71dd95571
plisek1986 <pawel.lisek.fp@gmail.com>
2021-02-17 06:28:00 +0100

    Defining user_input function

---
+++ Exercise1_1.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+def get_number():
+    """
+    Returns: User input
+    """
+    while True:
+        try:
+            user_input = int(input('Please provide number: '))
+            break
+        except ValueError:
+            print('Provided value is not a number, please try again!')
+    return user_input
